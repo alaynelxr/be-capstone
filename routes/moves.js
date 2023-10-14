98efe245a8d04982421400a35cc7036f3acd9329
alaynelxr <alaynelxr@gmail.com>
2023-10-14 12:06:38 +0800

    changed get all moves to include search query

--- routes/moves.js
+++ routes/moves.js
@@ -6,14 +6,60 @@ router.get("/test", (req, res) => {
 });
 
 // GET ALL MOVES at /moves
+// router.get("/", async (req, res) => {
+//   try {
+//     const moves = await prisma.move.findMany({
+//       include: {
+//         difficulty: true, // Include the 'difficulty' relation
+//         categories: true, // Include the 'category' relation
+//       },
+//     });
+//     res.status(200).json(moves);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: "Something went wrong! Status 500" });
+//   }
+// });
+
+// NEW GET moves with query parameters
 router.get("/", async (req, res) => {
+  const { query } = req.query;
+
   try {
-    const moves = await prisma.move.findMany({
-      include: {
-        difficulty: true, // Include the 'difficulty' relation
-        categories: true, // Include the 'category' relation
-      },
-    });
+    let moves;
+    if (query) {
+      moves = await prisma.move.findMany({
+        where: {
+          OR: [
+            {
+              title: {
+                contains: query,
+                mode: "insensitive",
+              },
+            },
+            {
+              aliases: {
+                contains: query,
+                mode: "insensitive",
+              },
+            },
+          ],
+        },
+        include: {
+          difficulty: true,
+          categories: true,
+        },
+      });
+    } else {
+      // If there's no query, return all data
+      moves = await prisma.move.findMany({
+        include: {
+          difficulty: true,
+          categories: true,
+        },
+      });
+    }
+
     res.status(200).json(moves);
   } catch (err) {
     console.error(err);
